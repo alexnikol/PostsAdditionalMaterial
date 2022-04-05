16d854f0965117151c96f7a0363ee5dc31d88f53
Alexander nikolaichuk <a.nikolaichuk@mediatech.ai>
2022-04-05 09:27:20 +0300

    InMemory cache added

--- PostsAdditionalMaterial/BooksService.swift
+++ PostsAdditionalMaterial/BooksService.swift
@@ -6,36 +6,16 @@
 //
 
 import Foundation
+import RealmSwift
 
-struct Book: Decodable {
+struct Book: Object, Decodable {
     let ID: UUID
     let title: String
     let author: String
 }
 
-class BooksService {
-    
-    private var networkProvider: NetworkProvider
-        
-    init(networkProvider: NetworkProvider) {
-        self.networkProvider = networkProvider
-    }
-    
-    func loadList(completion: @escaping (Result<[Book], Error>) -> Void) {
-        networkProvider.fetchBooks(request: .getBook, responseType: [Book].self) { result in
-            switch result {
-            case .success(let books):
-                completion(.success(books))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
-
 //class BooksService {
 //
-//    var cachedList: [Book] = []
 //    private var networkProvider: NetworkProvider
 //
 //    init(networkProvider: NetworkProvider) {
@@ -43,20 +23,41 @@ class BooksService {
 //    }
 //
 //    func loadList(completion: @escaping (Result<[Book], Error>) -> Void) {
-//        if cachedList.isEmpty {
-//            networkProvider.fetchBooks(request: .getBook, responseType: [Book].self) { result in
-//                switch result {
-//                case .success(let books):
-//                    self.cachedList = books
-//                    completion(.success(books))
-//                case .failure(let error):
-//                    completion(.failure(error))
-//                }
+//        networkProvider.fetchBooks(request: .getBook, responseType: [Book].self) { result in
+//            switch result {
+//            case .success(let books):
+//                completion(.success(books))
+//            case .failure(let error):
+//                completion(.failure(error))
 //            }
-//        } else {
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-//                completion(.success(self.cachedList))
-//            })
 //        }
 //    }
 //}
+
+class BooksService {
+
+    var cachedList: [Book] = []
+    private var networkProvider: NetworkProvider
+
+    init(networkProvider: NetworkProvider) {
+        self.networkProvider = networkProvider
+    }
+
+    func loadList(completion: @escaping (Result<[Book], Error>) -> Void) {
+        if cachedList.isEmpty {
+            networkProvider.fetchBooks(request: .getBook, responseType: [Book].self) { result in
+                switch result {
+                case .success(let books):
+                    self.cachedList = books
+                    completion(.success(books))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                completion(.success(self.cachedList))
+            })
+        }
+    }
+}
